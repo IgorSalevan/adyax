50c182aa30aedd64cc3bce15b20b9003bf3e6096
Anton Tsymuk <atsymuk@gmail.com>
2018-10-19 13:19:46 +0300

    fix typo

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const texts = {
   slogan: 'A Better Experience',
-  position: 'Frond-End Developer',
+  position: 'Front-End Developer',
   annonce: `Adyax’s core values take top priority: we care for our client, and we care for our people.
 Staff and clients work in partnership with consistent,  transparent communication.`,
   resume: `Adyax specializes in working with multi-national, multi-brand companies on a wide range of
